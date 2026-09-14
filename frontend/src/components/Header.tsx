6b3b6cf474dc9bbe0cbe0b58b80092ba9af7b878
elyasaf waisman <elyasafw1997@gmail.com>
2026-09-14 11:50:10 +0300

    add search link in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,8 +6,9 @@ const Header = () => {
             <h1>ClimaBoard</h1>
             <nav>
                 <NavLink to={"/"}>Home</NavLink>
-                <NavLink to={"/favorites"}>Favorites</NavLink>
+                <NavLink to={"/search"}>Search</NavLink>
                 <NavLink to={"/compare"}>Compare</NavLink>
+                <NavLink to={"/favorites"}>Favorites</NavLink>
             </nav>
         </header>
     );
